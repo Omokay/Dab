427c2a6ba86e7d4b56ed3aeda1204623a3bfde62
Omokay <chuku.omoke@gmail.com>
2020-07-05 00:23:42 +0100

    more changes to footer

--- src/Components/Footer/footer.component.jsx
+++ src/Components/Footer/footer.component.jsx
@@ -5,14 +5,16 @@ import { Facebook } from 'react-feather';
 import { Twitter } from 'react-feather';
 import { Linkedin } from 'react-feather';
 import { Instagram } from 'react-feather';
-import { Youtube } from 'react-feather';
+import { Phone } from 'react-feather';
+import { Mail } from 'react-feather';
+import { MapPin } from 'react-feather';
 
 
 const Footer = () => {
     return (
         <div className='footer'>
             <div className='row footer-row'>
-                <div className='columns col-md-3  col-sm-6'>
+                <div className='columns col-md-4  col-sm-6'>
                     <h3>Connect with us</h3>
                     <div className='sm-list'>
                         <Facebook className='icons' />
@@ -21,7 +23,7 @@ const Footer = () => {
                         <Instagram className='icons' />
                     </div>
                 </div>
-                <div className='columns col-md-3  col-sm-6'>
+                <div className='columns col-md-4  col-sm-6'>
                     <h3>Quick Links</h3>
                     <ul>
                         <li><a>Buy Airtime</a></li>
@@ -32,14 +34,11 @@ const Footer = () => {
 
                     </ul>
                 </div>
-                <div className='columns col-md-3  col-sm-6'>
+                <div className='columns col-md-4  col-sm-6'>
                     <h3>Contact us</h3>
-                    <p><span>Tel:</span>080-XXXX-XXXXX</p>
-                    <p><span>Email:</span>info@dab.com</p>
-                </div>
-                <div className='columns col-md-3  col-sm-6'>
-                    <h3>Address</h3>
-                    <p>No 1 Akin Lakanu Close Adeniyi Jones</p>
+                    <p><span><Phone />  </span>080-XXXX-XXXXX</p>
+                    <p><span><Mail />  </span>info@dab.com</p>
+                    <p><span><MapPin />  </span>No 1 Akin Lakanu Close Adeniyi Jones</p>
                 </div>
             </div>
             <p className='text-center text-copyright'>Built with &#128151; by Dab <span>&#169;</span> Copyright 2020. All rights reserved</p>
